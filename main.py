00af502bb0c795c77707eed1ea412644fb14bf08
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-03-22 02:59:45 +0100

    Last Commit

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 import pickle
 import json
+import os
+import psycopg2
+import sqlalchemy
+import databases
+
 
 
 
@@ -48,12 +53,31 @@ print('Accuracy score of the test data : ', test_data_accuracy)
 filename = 'diabetes_model.sav'
 pickle.dump(classifier, open(filename, 'wb'))
 
+DATABASE_URL = "postgresql://zkyaxnqrahshaq:f036cf58b6a8b19d7973e86c6315c3d06b349d95c460fbdbbf6f9d2e918a72a7@ec2-3-93-160-246.compute-1.amazonaws.com:5432/d4oiiepvk4n1bm"
 
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+
+user = sqlalchemy.Table(
+    "user",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("username", sqlalchemy.String),
+    sqlalchemy.Column("password", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+metadata.create_all(engine)
 # Set up the database connection
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Gbogo321@localhost/Diabetes_db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Gbogo321@localhost/Diabetes_db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
 origins = [
 "http://192.168.43.177:8000"
@@ -72,30 +96,37 @@ class model_input(BaseModel):
     Age : int      
 
 # Define the User model
-class User(Base):
-    __tablename__ = "userss"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+# class User(Base):
+#     __tablename__ = "userss"
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String, unique=True, index=True)
+#     email = Column(String, unique=True, index=True)
+#     password = Column(String)
 
-class Feed(Base):
-    __tablename__ = "feed"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    message1 = Column(String, index=True )
-    message2 = Column(String, index=True )
-    message3 = Column(String, index=True )
+# class Feed(Base):
+#     __tablename__ = "feed"
+#     id = Column(Integer, primary_key=True, index=True)
+#     email = Column(String, unique=True, index=True)
+#     message1 = Column(String, index=True )
+#     message2 = Column(String, index=True )
+#     message3 = Column(String, index=True )
 
     
 # Create the tables in the database
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 # Define the request body schema
 class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+class UserCreates(BaseModel):
+    id: int
+    username: str
+    email: str
+    password: str
+ 
 
 class UserLogin(BaseModel):
     email: str
@@ -117,136 +148,148 @@ diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 app = FastAPI()
 #for signup page
 # Define the signup endpoint
+@app.onevent('startup')
+async def startup():
+    await database.connect()
+
+@app.onevent('startup')
+async def startup():
+    await database.connect()
+        
 @app.post("/signup/")
-def signup(user: UserCreate):
+async def signup(user: UserCreate):
+    query = user.insert().values(username=user.username, password=user.password)
+    last_record_id = await database.execute(query)
+    return{"message":"Signup Successful"}
+    # await
     # Create a new user object from the request body
-    db_user = User(username=user.username, email=user.email, password=user.password)
+    # db_user = User(username=user.username, email=user.email, password=user.password)
     
     # Add the user to the database
-    db = SessionLocal()
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    # db = SessionLocal()
+    # db.add(db_user)
+    # db.commit()
+    # db.refresh(db_user)
     
     # Return the newly created user
-    return {"message": db_user}
+    # return {"message": db_user}
 
 
-@app.get("/")
-async def root():
-    raise HTTPException(status_code=404, detail="page not found")
-    return {}
+# @app.get("/")
+# async def root():
+#     raise HTTPException(status_code=404, detail="page not found")
+#     return {}
 
-#for login page
-@app.post("/login/")
-def login(user: UserLogin):
-    # Get the user from the database by email
-    db = SessionLocal()
-    db_user = db.query(User).filter(User.email == user.email).first()
+# #for login page
+# @app.post("/login/")
+# def login(user: UserLogin):
+#     # Get the user from the database by email
+#     db = SessionLocal()
+#     db_user = db.query(User).filter(User.email == user.email).first()
     
-    # Check if the user exists and the password is correct
-    if not db_user or db_user.password != user.password:
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+#     # Check if the user exists and the password is correct
+#     if not db_user or db_user.password != user.password:
+#         raise HTTPException(status_code=401, detail="Invalid email or password")
     
-    # Return the user
-    return {"message": "successful"}
+#     # Return the user
+#     return {"message": "successful"}
 
 
-class Profile(BaseModel):
-    name: str
-    email: str
-    bio: str
+# class Profile(BaseModel):
+#     name: str
+#     email: str
+#     bio: str
 
 
-#for profile page
-@app.put("/profile/")
-async def create_profile(profile: Profile, profile_pic: UploadFile = File(...)):
+# #for profile page
+# @app.put("/profile/")
+# async def create_profile(profile: Profile, profile_pic: UploadFile = File(...)):
 
-    return {"profile": profile, "profile_pic_filename": profile_pic.filename}
-@app.get("/profile/")
-def get_profiles():
-    db = SessionLocal()
-    profiles = db.query(User.email,User.username,)
-    db.close()
-    return{"message": profiles}
-class PasswordResetRequest(BaseModel):
-    email: str
+#     return {"profile": profile, "profile_pic_filename": profile_pic.filename}
+# @app.get("/profile/")
+# def get_profiles():
+#     db = SessionLocal()
+#     profiles = db.query(User.email,User.username,)
+#     db.close()
+#     return{"message": profiles}
+# class PasswordResetRequest(BaseModel):
+#     email: str
 
-class UserOut(BaseModel):
-    username: str
-    email: str
+# class UserOut(BaseModel):
+#     username: str
+#     email: str
 
 
-password_reset_requests = []
+# password_reset_requests = []
 
-@app.post("/forgot-password/")
-async def request_password_reset(request: PasswordResetRequest):
-    # Retrieve user with matching email from database
-    db = SessionLocal()
-    user = db.query(User).filter(User.email == request.email).first()
-    db.close()
+# @app.post("/forgot-password/")
+# async def request_password_reset(request: PasswordResetRequest):
+#     # Retrieve user with matching email from database
+#     db = SessionLocal()
+#     user = db.query(User).filter(User.email == request.email).first()
+#     db.close()
 
-    if user:
-        # Send password reset email to the user's email address
-        return {"message": "Password reset email sent"}
-    else:
-        return {"message": "User not found"}
+#     if user:
+#         # Send password reset email to the user's email address
+#         return {"message": "Password reset email sent"}
+#     else:
+#         return {"message": "User not found"}
 
-@app.get("/users/")
-async def list_users():
-    # Retrieve all users from database
-    db = SessionLocal()
-    users = db.query(User).all()
-    db.close()
+# @app.get("/users/")
+# async def list_users():
+#     # Retrieve all users from database
+#     db = SessionLocal()
+#     users = db.query(User).all()
+#     db.close()
 
-    # Return list of UserOut objects
-    return [UserOut(username=user.username, email=user.email, id=user.id) for user in users]
+#     # Return list of UserOut objects
+#     return [UserOut(username=user.username, email=user.email, id=user.id) for user in users]
 
-@app.delete("/users/{user_email}")
-async def delete_user(user_email: str):
-    # Delete user with given ID from database
-    db = SessionLocal()
-    user = db.query(User).filter(User.email == user_email).first()
-    db.delete(user)
-    db.commit()
-    db.close()
+# @app.delete("/users/{user_email}")
+# async def delete_user(user_email: str):
+#     # Delete user with given ID from database
+#     db = SessionLocal()
+#     user = db.query(User).filter(User.email == user_email).first()
+#     db.delete(user)
+#     db.commit()
+#     db.close()
 
-    return {"message": "User deleted"}
+#     return {"message": "User deleted"}
 
-@app.post("/feedback/")
-def Feedback(feed_back: Feedbacks):
-      db = SessionLocal()
-      db_feedback = Feed(feed_back.email,feed_back.message1,feed_back.message2,feed_back.message3) 
-      db.add(db_feedback)
-      db.commit()
-      db.refresh(db_feedback)
-      return {"message": "Thank you for your feedback!"}
+# @app.post("/feedback/")
+# def Feedback(feed_back: Feedbacks):
+#       db = SessionLocal()
+#       db_feedback = Feed(feed_back.email,feed_back.message1,feed_back.message2,feed_back.message3) 
+#       db.add(db_feedback)
+#       db.commit()
+#       db.refresh(db_feedback)
+#       return {"message": "Thank you for your feedback!"}
 
-@app.post('/diabetes_prediction')
-def diabetes_predd(input_parameters : model_input):
+# @app.post('/diabetes_prediction')
+# def diabetes_predd(input_parameters : model_input):
     
-    input_data = input_parameters.json()
-    input_dictionary = json.loads(input_data)
+#     input_data = input_parameters.json()
+#     input_dictionary = json.loads(input_data)
     
-    preg = input_dictionary['pregnancies']
-    glu = input_dictionary['Glucose']
-    bp = input_dictionary['BloodPressure']
-    skin = input_dictionary['SkinThickness']
-    insulin = input_dictionary['Insulin']
-    bmi = input_dictionary['BMI']
-    dpf = input_dictionary['DiabetesPedigreeFunction']
-    age = input_dictionary['Age']
+#     preg = input_dictionary['pregnancies']
+#     glu = input_dictionary['Glucose']
+#     bp = input_dictionary['BloodPressure']
+#     skin = input_dictionary['SkinThickness']
+#     insulin = input_dictionary['Insulin']
+#     bmi = input_dictionary['BMI']
+#     dpf = input_dictionary['DiabetesPedigreeFunction']
+#     age = input_dictionary['Age']
     
     
-    input_list = [preg, glu, bp, skin, insulin, bmi, dpf, age]
+#     input_list = [preg, glu, bp, skin, insulin, bmi, dpf, age]
     
-    prediction = diabetes_model.predict([input_list])
+#     prediction = diabetes_model.predict([input_list])
     
-    if (prediction == 0):
-        return 'The person is not diabetic'
-    else:
-        return 'The person is diabetic'
-    return{"message" : "successful"} 
+#     if (prediction == 0):
+#         return 'The person is not diabetic'
+#     else:
+#         return 'The person is diabetic'
+#     return{"message" : "successful"} 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
