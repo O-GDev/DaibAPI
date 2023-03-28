13fbd745baeabe5148d9cca5ca0aafa9cce682d4
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-03-28 10:02:43 +0100

    last commit

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def get_profiles(userP: Profiles):
     # query = user.insert().values(email=userP.email)
     profiles = user.select().where(userP.email == user.c.email)
     db_profiles_ = await database.fetch_one(profiles)
-    return{"last_name": db_profiles_.last_name,"first_name": db_profiles_.first_name,"email": db_profile_.email}
+    return{"last_name": db_profiles_.last_name,"first_name": db_profiles_.first_name,"email": db_profiles_.email}
 class PasswordResetRequest(BaseModel):
     email: str
 
