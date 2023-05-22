d17197ff5fb58b0fdf9a5fc1f92d6c9fb5769d63
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-05-22 18:05:41 +0100

    adjustment

--- schemas.py
+++ schemas.py
@@ -38,7 +38,8 @@ class UserLogin(BaseModel):
 
 # Login response model
 class UserResponse(BaseModel):
-    email: str
+    username: str
+    status: str
     class Config:
         orm_mode = True  
 
