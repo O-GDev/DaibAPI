1b4b63f8f9f7e0dfaa217afd10a9cf7b9261cb47
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-06-16 22:16:29 +0100

    adjustment

---
+++ alembic/versions/53f26b3890f8_create_users_table.py
@@ -0,0 +1,24 @@
+"""create users table
+
+Revision ID: 53f26b3890f8
+Revises: 
+Create Date: 2023-06-16 22:14:49.362709
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '53f26b3890f8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ alembic/versions/69be33196a71_create_feedback_table.py
@@ -0,0 +1,24 @@
+"""create feedback table
+
+Revision ID: 69be33196a71
+Revises: 53f26b3890f8
+Create Date: 2023-06-16 22:15:49.671907
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '69be33196a71'
+down_revision = '53f26b3890f8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
