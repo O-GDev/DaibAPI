c26b661c10134443c2710197e85690033743c053
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-06-16 22:23:28 +0100

    adjustment

--- alembic/versions/53f26b3890f8_create_users_table.py
+++ alembic/versions/53f26b3890f8_create_users_table.py
@@ -17,8 +17,24 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.create_table('users',
+                    sa.Column('id', sa.Integer(), nullable=False, ),
+                    sa.Column('first_name', sa.String(), nullable=False),
+                    sa.Column('last_name', sa.String(), nullable=False),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('token', sa.String()),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('occupation', sa.String(), nullable=False),
+                    sa.Column('house_address', sa.String(), nullable=False),
+                    sa.Column('phone_number', sa.String(), nullable=False),
+                    sa.Column('profile_pics', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=True, server_default=sa.text('now()')),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email')
+                    )
     pass
 
 
 def downgrade() -> None:
+    op.drop_table("users")    
     pass
--- alembic/versions/69be33196a71_create_feedback_table.py
+++ alembic/versions/69be33196a71_create_feedback_table.py
@@ -17,8 +17,16 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.create_table('feedback',
+                    sa.Column('id', sa.Integer(), nullable=False, ),
+                    sa.Column('message1', sa.String(), nullable=False),
+                    sa.Column('message2', sa.String(), nullable=False),
+                    sa.Column('message3', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=True, server_default=sa.text('now()')),
+                    )
     pass
 
 
 def downgrade() -> None:
+    op.drop_table('feedback')
     pass
